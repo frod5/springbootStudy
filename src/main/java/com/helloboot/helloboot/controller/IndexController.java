b7a7b2efe66d977b996fd6407ca4f0efde08265c
sunwooKim <frod5@kakao.com>
2023-02-24 23:00:37 +0900

    spring boot 강의 3일차 - service 생성

--- src/main/java/com/helloboot/helloboot/controller/IndexController.java
+++ src/main/java/com/helloboot/helloboot/controller/IndexController.java
@@ -1,16 +1,20 @@
 package com.helloboot.helloboot.controller;
 
+import com.helloboot.helloboot.service.SimpleHelloService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 //@RequestMapping({"/v1/test"})
 //@RestController
 public class IndexController {
     //@GetMapping("/{name}")
     public String hello(@PathVariable String name) {
-        return name;
+        SimpleHelloService simpleHelloService = new SimpleHelloService();
+        return simpleHelloService.sayHello(Objects.requireNonNull(name));
     }
 }
---
+++ src/main/java/com/helloboot/helloboot/service/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package com.helloboot.helloboot.service;
+
+public class SimpleHelloService {
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+}

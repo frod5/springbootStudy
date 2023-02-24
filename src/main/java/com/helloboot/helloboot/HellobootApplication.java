d7f2e733507f986c3f3b3c6843ba718cc4423b64
sunwooKim <frod5@kakao.com>
2023-02-25 00:15:48 +0900

    spring boot 강의 3일차 - @Configuration 어노테이션이 붙으면 WebApplicationContext에 처음으로 등록되는걸 확인하기 위한 소스작업

--- src/main/java/com/helloboot/helloboot/HellobootApplication.java
+++ src/main/java/com/helloboot/helloboot/HellobootApplication.java
@@ -1,6 +1,7 @@
 package com.helloboot.helloboot;
 
 import com.helloboot.helloboot.controller.IndexController;
+import com.helloboot.helloboot.service.HelloService;
 import com.helloboot.helloboot.service.v1.impl.SimpleHelloService;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
@@ -8,11 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -23,13 +28,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+@Configuration
 public class HellobootApplication {
+    @Bean
+    public IndexController indexController(HelloService helloService) {
+        return new IndexController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
 
         //Spring Container 생성
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -44,8 +58,7 @@ public class HellobootApplication {
             }
         };
 
-        applicationContext.registerBean(IndexController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.register(HellobootApplication.class);
         applicationContext.refresh();
     }
 

e149a3b3a588d24b54fd7d4cf9cb972f5d698e7f
sunwooKim <frod5@kakao.com>
2023-02-26 20:40:54 +0900

    spring boot 강의 4일차 - SpringBoot 기본 SpringAppliction.run()으로 변경

--- src/main/java/com/helloboot/helloboot/HellobootApplication.java
+++ src/main/java/com/helloboot/helloboot/HellobootApplication.java
@@ -1,12 +1,11 @@
 package com.helloboot.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -25,6 +24,6 @@ public class HellobootApplication {
     public static void main(String[] args) {
 
         //Spring Container 생성
-        MySpringApplication.run(HellobootApplication.class, args);
+        SpringApplication.run(HellobootApplication.class, args);
     }
 }
